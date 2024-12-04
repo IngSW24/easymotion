1825f018ab8e649a6b26ff1e151cd9c269edd616
Arghittu Thomas <66645018+ArghittuThms@users.noreply.github.com>
2024-12-04 17:31:59 +0100

    add Prisma seeding

---
+++ api/prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+const dt = require("./seed_data.json");
+console.log(dt);
+
+await dt.event.forEach(async (e) => {
+await prisma.event.upsert({
+where: { id : e.id },
+update: {},
+create: {
+id : e.id,
+organizer : e.organizer,
+instructor : e.instructor,
+type : e.type,
+description : e.description,
+location : e.location,
+frequency : e.frequency,
+times : e.times,
+cost : e.cost
+}
+});
+});
+}
+
+main()
+.catch((e) => {
+console.error(e);
+process.exit(1);
+})
+.finally(async () => {
+await prisma.$disconnect();
+});
