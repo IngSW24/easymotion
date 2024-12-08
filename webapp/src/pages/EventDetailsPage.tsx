43a3fdd6e83e27bdfa0787fda2e429f1ea8516d1
Nicola Revelant <nicolarevelant@outlook.com>
2024-12-08 23:03:56 +0100

    Show no elements found instead of a blank screen, and minor fixes

--- webapp/src/pages/EventDetailsPage.tsx
+++ webapp/src/pages/EventDetailsPage.tsx
@@ -1,7 +1,8 @@
-import { Link, useParams } from "react-router";
+import { Link, useNavigate, useParams } from "react-router";
 import { EventEntity } from "../data/event";
-import { useEffect, useState } from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Fragment, useEffect, useState } from "react";
+import { Button, Typography } from "@mui/material";
+import DeleteButton from "../components/DeleteButton";
 
 const API_URL = "https://api.easymotion.devlocal"
 
@@ -12,46 +13,61 @@ const defaultEvent: EventEntity | null = null
  * This is supposed to fit the entire screen
  */
 export default function EventDetailsPage() {
-  const {id} = useParams()
+  const {id} = useParams<string>()
   const [eventDetails, setEventDetails] = useState(defaultEvent)
+  const navigate = useNavigate()
+
   useEffect(() => {
     let accept = true
 
     function fetchEventDetails() {
       fetch(API_URL + "/events/" + id).then(response => {
+        if (response.status !== 200) {
+          throw new Error("Response status: " + response.status)
+        }
         response.json().then(json => {
-        if (accept)
-          setEventDetails(json)
+          if (accept)
+            setEventDetails(json)
         })
-      }).catch(console.error)
+      }).catch((reason) => {
+        console.error(reason)
+        navigate("/")
+      })
     }
 
-    fetchEventDetails()
+    if (id)
+      fetchEventDetails()
+    else
+      navigate("/")
 
     return () => {
       accept = false
     }
-  }, [id])
+  }, [id, navigate])
   
   return (
-    <Box>
+    <Fragment>
       <Button><Link to={"/"}>HOME</Link></Button>
-      { eventDetails ? (
-        <Typography variant="h4" display="block">
-          <ul>
-            <li>Organizer: {eventDetails.organizer}</li>
-            <li>Instructor: {eventDetails.instructor}</li>
-            <li>Type: {eventDetails.type}</li>
-            <li>Description: {eventDetails.description}</li>
-            <li>Location: {eventDetails.location}</li>
-            <li>Frequency: {eventDetails.frequency}</li>
-            <li>Times: {eventDetails.times}</li>
-            <li>Cost: {eventDetails.cost}</li>
-          </ul>
-        </Typography>
-      ) : (
-        <Typography align="center" variant="h1" display="block">Loading...</Typography>
-      )
-    }
-    </Box>)
+      { (eventDetails && id) ? (
+        <Fragment>
+          <Typography variant="h4" display="block">
+            <ul>
+              <li>Organizer: {eventDetails.organizer}</li>
+              <li>Instructor: {eventDetails.instructor}</li>
+              <li>Type: {eventDetails.type}</li>
+              <li>Description: {eventDetails.description}</li>
+              <li>Location: {eventDetails.location}</li>
+              <li>Frequency: {eventDetails.frequency}</li>
+              <li>Times: {eventDetails.times}</li>
+              <li>Cost: {eventDetails.cost}</li>
+            </ul>
+          </Typography>
+          <DeleteButton id={id} />
+        </Fragment>
+        ) : (
+          <Typography align="center" variant="h2" display="block">Loading...</Typography>
+        )
+      }
+    </Fragment>
+  )
 }
--- webapp/src/pages/EventsPage.tsx
+++ webapp/src/pages/EventsPage.tsx
@@ -1,18 +1,21 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import EventCard from "../components/EventCard";
 import { EventsEntity } from "../data/events"
 import { Grid2, Typography } from "@mui/material";
+import { cancellaEvento } from "../components/DeleteButton";
+import DeleteDialog from "../components/DeleteDialog";
 
 const API_URL = "https://api.easymotion.devlocal"
-
-const defaultEvents: EventsEntity | null = null
 
 /**
  * Events page, which shows a grid of events
  * @returns a react component
  */
 export default function EventsPage() {
-  const [events, setEvents] = useState(defaultEvents)
+  const [events, setEvents] = useState<EventsEntity | null>(null)
+
+  const [id, setId] = useState<string | null>(null);
+
   useEffect(() => {
     let accept = true
 
@@ -32,25 +35,33 @@ export default function EventsPage() {
     }
   }, [])
 
-  function onEventCardClick(id: string) {
-    console.log("Hai premuto tasto Delete sull'evento: " + id)
+  function onCardDeleteClick(id: string) {
+    setId(id)
   }
 
   if (events) {
-    return (
-      <Grid2 container spacing={6}>
-        {events.data.map((e) =>
-          <Grid2 key={e.id}>
-            <EventCard event={e} onDeleteClick={() => onEventCardClick(e.id)} />
+    if (events.data.length > 0) {
+      return (
+        <Fragment>
+          <Grid2 container spacing={6}>
+            {events.data.map((e) =>
+              <Grid2 key={e.id}>
+                <EventCard event={e} onDeleteClick={() => onCardDeleteClick(e.id)} />
+              </Grid2>
+              )
+            }
           </Grid2>
-          )
-        }
-      </Grid2>
-    )
+          <DeleteDialog id={id} handleCloseAnnulla={() => setId(null)} handleCloseCancella={() => cancellaEvento(id)} />
+        </Fragment>
+      )
+    } else {
+      return (
+        <Typography align="center" variant="h2" display="block">No elements found</Typography>
+      )
+    }
   } else {
     return (
-      <Typography align="center" variant="h1" display="block">Loading...</Typography>
+      <Typography align="center" variant="h2" display="block">Loading...</Typography>
     )
   }
-  
-};
+}
