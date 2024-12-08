c5acebab29ff6c775b6671be9c917aac9cf3ac3c
PittMatteo <112318700+PittMatteo@users.noreply.github.com>
2024-12-08 17:40:36 +0100

    feat: edit details

--- webapp/src/pages/EventDetailsPage.tsx
+++ webapp/src/pages/EventDetailsPage.tsx
@@ -4,51 +4,50 @@ import { Fragment, useEffect, useState } from "react";
 import { Button, Typography } from "@mui/material";
 import DeleteButton from "../components/DeleteButton";
 
-const API_URL = "https://api.easymotion.devlocal"
-
-const defaultEvent: EventEntity | null = null
+const defaultEvent: EventEntity | null = null;
 
 /**
  * EventDetailsPage shows information about an event
  * This is supposed to fit the entire screen
  */
 export default function EventDetailsPage() {
-  const {id} = useParams<string>()
-  const [eventDetails, setEventDetails] = useState(defaultEvent)
-  const navigate = useNavigate()
+  const { id } = useParams<string>();
+  const [eventDetails, setEventDetails] = useState(defaultEvent);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    let accept = true
+    let accept = true;
 
     function fetchEventDetails() {
-      fetch(API_URL + "/events/" + id).then(response => {
-        if (response.status !== 200) {
-          throw new Error("Response status: " + response.status)
-        }
-        response.json().then(json => {
-          if (accept)
-            setEventDetails(json)
+      fetch(process.env.VITE_API_URL + "/events/" + id)
+        .then((response) => {
+          if (response.status !== 200) {
+            throw new Error("Response status: " + response.status);
+          }
+          response.json().then((json) => {
+            if (accept) setEventDetails(json);
+          });
         })
-      }).catch((reason) => {
-        console.error(reason)
-        navigate("/")
-      })
+        .catch((reason) => {
+          console.error(reason);
+          navigate("/");
+        });
     }
 
-    if (id)
-      fetchEventDetails()
-    else
-      navigate("/")
+    if (id) fetchEventDetails();
+    else navigate("/");
 
     return () => {
-      accept = false
-    }
-  }, [id, navigate])
-  
+      accept = false;
+    };
+  }, [id, navigate]);
+
   return (
     <Fragment>
-      <Button><Link to={"/"}>HOME</Link></Button>
-      { (eventDetails && id) ? (
+      <Button>
+        <Link to={"/"}>HOME</Link>
+      </Button>
+      {eventDetails && id ? (
         <Fragment>
           <Typography variant="h4" display="block">
             <ul>
@@ -64,10 +63,11 @@ export default function EventDetailsPage() {
           </Typography>
           <DeleteButton id={id} />
         </Fragment>
-        ) : (
-          <Typography align="center" variant="h2" display="block">Loading...</Typography>
-        )
-      }
+      ) : (
+        <Typography align="center" variant="h2" display="block">
+          Loading...
+        </Typography>
+      )}
     </Fragment>
-  )
+  );
 }
