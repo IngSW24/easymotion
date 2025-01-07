5cd3e5fa2a119c69f5ce5339945e87d8c63a54ed
canta2899 <andreacantarutti@icloud.com>
2025-01-07 23:39:44 +0100

    feat: monorepo with dev containers

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,14 +1,3 @@
-import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
-import {
-  API_DESCRIPTION,
-  API_TITLE,
-  API_VERSION,
-} from './common/constants/swagger.constants';
-import { PrismaClientExceptionFilter } from 'nestjs-prisma';
-import { ConfigService } from '@nestjs/config';
 import { bootstrap, setupSwagger } from './bootstrap';
 
 async function start() {
---
+++ webapp/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+# Generates self signed SSL certificates for development
+# An entry for DOMAIN should be added to /etc/hosts
+
+
+exec "$@"
