1802a402a82604dd4e12d0ee70f3e73de4322186
Arghittu Thomas <66645018+ArghittuThms@users.noreply.github.com>
2025-01-09 17:12:02 +0100

    create auth and users nest modules

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CourseModule } from './courses/courses.module';
 import { PrismaModule } from 'nestjs-prisma';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), CourseModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), CourseModule, PrismaModule, AuthModule, UsersModule],
 })
 export class AppModule {}
