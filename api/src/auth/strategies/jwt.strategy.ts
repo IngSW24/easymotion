16e62e8db8699f21ccd4fe89a45d423ca1a500b8
canta2899 <andreacantarutti@icloud.com>
2025-01-12 03:30:35 +0000

    fix: removed non needed dependency in jwt strategy

--- api/src/auth/strategies/jwt.strategy.ts
+++ api/src/auth/strategies/jwt.strategy.ts
@@ -1,6 +1,5 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from '../auth.service';
 import { ConfigType } from '@nestjs/config';
 import { Inject } from '@nestjs/common';
 import jwtConfig from 'src/config/jwt.config';
@@ -9,7 +8,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     @Inject(jwtConfig.KEY)
     config: ConfigType<typeof jwtConfig>,
-    private readonly authService: AuthService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
