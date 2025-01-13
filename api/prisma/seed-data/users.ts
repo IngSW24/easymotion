3a420683b99af01ff45d0f985bfcb6d86169610c
canta2899 <andreacantarutti@icloud.com>
2025-01-13 00:00:40 +0000

    feat: updated seeding

--- api/prisma/seed-data/users.ts
+++ api/prisma/seed-data/users.ts
@@ -1,17 +1,33 @@
 import { Prisma } from '@prisma/client';
 import * as argon2 from 'argon2';
+import { v4 } from 'uuid';
 
 const getUsers = async (): Promise<Prisma.ApplicationUserCreateInput[]> => {
   const passwordHash = await argon2.hash('password');
   return [
     {
-      id: '1a2b3c4d-5678-9abc-def0-123456789abc',
-      email: 'superadmin@easymotion.it',
+      id: v4(),
+      email: 'admin@easymotion.it',
       passwordHash,
-      username: 'superadmin',
-      firstName: 'Super',
+      username: 'admin',
+      firstName: 'Admin',
       middleName: null,
-      lastName: 'Admin',
+      lastName: 'User',
+      phoneNumber: '+393401234567',
+      birthDate: '1990-05-15',
+      role: 'ADMIN',
+      isEmailVerified: true,
+      lastLogin: '2025-01-08T10:00:00.000Z',
+      failedLoginAttempts: 0,
+    },
+    {
+      id: v4(),
+      email: 'user@easymotion.it',
+      passwordHash,
+      username: 'customer',
+      firstName: 'Customer',
+      middleName: null,
+      lastName: 'User',
       phoneNumber: '+393401234567',
       birthDate: '1990-05-15',
       role: 'USER',
@@ -20,16 +36,16 @@ const getUsers = async (): Promise<Prisma.ApplicationUserCreateInput[]> => {
       failedLoginAttempts: 0,
     },
     {
-      id: '5f6g7h8i-9j0k-lmno-pqr1-2s3t4u5v6w7x',
-      email: 'superadmin2@easymotion.it',
+      id: v4(),
+      email: 'physiotherapist@easymotion.it',
       passwordHash,
-      username: 'superadmin2',
-      firstName: 'Super',
-      middleName: '2',
-      lastName: 'Admin',
+      username: 'physio',
+      firstName: 'Physhiotherapist',
+      middleName: null,
+      lastName: 'User',
       phoneNumber: '+393401234567',
       birthDate: '1990-05-15',
-      role: 'USER',
+      role: 'PHYSIOTHERAPIST',
       isEmailVerified: false,
       lastLogin: '2025-01-08T10:00:00.000Z',
       failedLoginAttempts: 0,
