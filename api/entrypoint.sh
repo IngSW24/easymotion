87fe0cda229f8114e5240f7694332fffe294f535
canta2899 <andreacantarutti@icloud.com>
2025-01-17 23:46:33 +0100

    feat: entrypoint copy and skippable auto-install

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -2,12 +2,16 @@
 set -e
 
 if [ -d "node_modules" ]; then
-    echo 'Installing dependencies...';
-    npm i;
+    if [ "${AUTO_INSTALL_DEPS:-true}" = "true" ]; then
+        echo 'Installing additional dependencies...'
+        npm i
+    else
+        echo 'Skipping additional dependency installation.'
+    fi
 else
-    echo 'Dependencies are not installed.';
-    echo 'Attempting to install dependencies... (this may take a while)';
-    npm ci;
+    echo 'Dependencies are not installed.'
+    echo 'Attempting to install dependencies... (this may take a while)'
+    npm ci
 fi
 
 exec "$@"
--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -2,17 +2,21 @@
 set -e
 
 if [ -d "node_modules" ]; then
-    echo 'Installing dependencies...';
-    npm i;
+    if [ "${AUTO_INSTALL_DEPS:-true}" = "true" ]; then
+        echo 'Installing additional dependencies...'
+        npm i
+    else
+        echo 'Skipping additional dependency installation.'
+    fi
 else
-    echo 'Dependencies are not installed.';
-    echo 'Attempting to install dependencies... (this may take a while)';
-    npm ci;
+    echo 'Dependencies are not installed.'
+    echo 'Attempting to install dependencies... (this may take a while)'
+    npm ci
 fi
 
 if [ ! -d "src/client" ]; then
     echo 'API client is missing.';
-    echo 'Attempting to generate API client... (this may take a while)';
+    echo 'Attempting to generate API client... (this may take a while)'
     npm run generate-client;
 fi
 
