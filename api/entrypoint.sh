6c670e674d698736d44405101f4f20e50cfbe62a
Nicola Revelant <nicolarevelant@outlook.com>
2025-01-17 10:50:53 +0100

    Auto-install deps when node_modules exists

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-if [ ! -d "node_modules" ]; then
+if [ -d "node_modules" ]; then
+    echo 'Installing dependencies...';
+    npm i;
+else
     echo 'Dependencies are not installed.';
     echo 'Attempting to install dependencies... (this may take a while)';
     npm ci;
--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-if [ ! -d "node_modules" ]; then
+if [ -d "node_modules" ]; then
+    echo 'Installing dependencies...';
+    npm i;
+else
     echo 'Dependencies are not installed.';
     echo 'Attempting to install dependencies... (this may take a while)';
     npm ci;
@@ -9,7 +12,7 @@ fi
 
 if [ ! -d "src/client" ]; then
     echo 'API client is missing.';
-    echo 'Attempting to generate API client';
+    echo 'Attempting to generate API client... (this may take a while)';
     npm run generate-client;
 fi
 
