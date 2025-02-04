6d82fb1b22e1c7e1557c4d3bff8652f113d895ac
canta2899 <andreacantarutti@icloud.com>
2025-02-04 13:21:41 +0100

    fix: api eslint config

--- packages/eslint-config/nest.js
+++ packages/eslint-config/nest.js
@@ -5,36 +5,22 @@ import prettier from "eslint-plugin-prettier";
 export default tseslint.config(
   { ignores: [".eslintrc.js"] },
   {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: "tsconfig.json",
-        tsconfigRootDir: import.meta.dirname,
-        sourceType: "module",
-      },
       ecmaVersion: 2020,
+      sourceType: "module",
     },
-
     plugins: {
-      "@typescript-eslint": tseslint.plugin,
       prettier,
     },
-
-    configs: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      prettier.configs.recommended,
-    ],
-
-    files: ["**/*.{ts,tsx}"],
-
-    // 🔹 Rules (Equivalent to your old config)
     rules: {
       "@typescript-eslint/interface-name-prefix": "off",
       "@typescript-eslint/explicit-function-return-type": "off",
       "@typescript-eslint/explicit-module-boundary-types": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": ["warn", { args: "none" }],
+      ...prettier.configs.recommended.rules,
     },
   }
 );
