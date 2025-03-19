4f4f28bab187630574bab5c557ee2ae89d9f6a53
canta2899 <andreacantarutti@icloud.com>
2025-03-19 22:16:01 +0100

    test(api): test suite for subscription service

--- api/src/subscriptions/subscriptions.controller.spec.ts
+++ api/src/subscriptions/subscriptions.controller.spec.ts
@@ -3,6 +3,8 @@ import { SubscriptionsController } from "./subscriptions.controller";
 import { SubscriptionsService } from "./subscriptions.service";
 import { PrismaService } from "nestjs-prisma";
 import { NotFoundException } from "@nestjs/common";
+import { CourseSubcriberDto } from "./dto/course-subcriber.dto";
+import { toPaginatedOutput } from "src/common/utils/pagination";
 
 describe("SubscriptionsController", () => {
   let controller: SubscriptionsController;
@@ -11,6 +13,7 @@ describe("SubscriptionsController", () => {
     getCustomerSubscriptions: jest.fn(),
     subscribeFinalUser: jest.fn(),
     unsubscribeFinalUser: jest.fn(),
+    getCourseSubscriptions: jest.fn(),
   };
 
   const prismaServiceMockup = {};
@@ -97,5 +100,62 @@ describe("SubscriptionsController", () => {
     expect(controller.subscribe(courseId, req)).rejects.toThrow(
       NotFoundException
     );
+  });
+
+  it("should unsubscribe a final user from a course", async () => {
+    const req = { user: { sub: "1" } };
+    const courseId = "1";
+
+    const result = await controller.unsubscribe(courseId, req);
+
+    subscriptionServiceMockup.unsubscribeFinalUser.mockReturnValue(undefined);
+
+    expect(subscriptionServiceMockup.unsubscribeFinalUser).toHaveBeenCalledWith(
+      req.user.sub,
+      courseId
+    );
+
+    expect(result).toBeUndefined();
+  });
+
+  it("should throw not found exception when user is not found on unsubscribe", async () => {
+    const req = { user: { sub: "1" } };
+    const courseId = "1";
+
+    subscriptionServiceMockup.unsubscribeFinalUser.mockRejectedValue(() => {
+      throw new NotFoundException("User not found");
+    });
+
+    expect(controller.unsubscribe(courseId, req)).rejects.toThrow(
+      NotFoundException
+    );
+  });
+
+  it("should get the list of subscriptions for a course", async () => {
+    const subscribers: CourseSubcriberDto[] = [
+      {
+        id: "1",
+        email: "email@email.email",
+        firstName: "a",
+        lastName: "b",
+        middleName: "c",
+        subscriptionDate: new Date(),
+      },
+    ];
+
+    const output = toPaginatedOutput(subscribers, 1, { page: 1, perPage: 10 });
+
+    subscriptionServiceMockup.getCourseSubscriptions.mockResolvedValue(output);
+
+    const result = await controller.getSubscribers("1", {
+      page: 1,
+      perPage: 10,
+    });
+
+    expect(result).toEqual(output);
+
+    expect(
+      subscriptionServiceMockup.getCourseSubscriptions
+    ).toHaveBeenCalledWith("1", { page: 1, perPage: 10 });
   });
 });
