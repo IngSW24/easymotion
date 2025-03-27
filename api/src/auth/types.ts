6e7a725956f27e020aed68526468c0ec5ea01d36
canta2899 <andreacantarutti@icloud.com>
2025-03-27 19:13:39 +0100

    feat(api): updated response structure on login

--- api/src/auth/types.ts
+++
@@ -1,9 +0,0 @@
-import { AuthUserDto } from "./dto/auth-user/auth-user.dto";
-
-export type TwoFactorDiscriminator =
-  | { requiresOtp: true }
-  | (LoginResponse & { requiresOtp: false });
-
-export type LoginResponse = {
-  user: AuthUserDto & { accessToken: string; refreshToken: string };
-};
