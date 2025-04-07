3b377dde53e57246051755c62a1beaa40277491a
PittMatteo <112318700+PittMatteo@users.noreply.github.com>
2025-04-07 17:28:43 +0200

    Feat/user page (#49)
    * fix: changes about header, text and path
    
    * fix: header text

--- webapp/src/components/Layout/headers/UserHeaders.tsx
+++ webapp/src/components/Layout/headers/UserHeaders.tsx
@@ -3,11 +3,11 @@ import ProfileButton from "../ProfileButton";
 
 export default function UserHeader() {
   return (
-    <AppBar position="fixed" sx={{ backgroundColor: "cyan" }}>
+    <AppBar position="fixed" sx={{ backgroundColor: "primary.main" }}>
       <Toolbar>
         {/* Your dashboard-specific header content */}
         <Typography variant="h6" noWrap component="div">
-          User Header
+          EasyMotion
         </Typography>
         <Box sx={{ flexGrow: 1 }} />
         <ProfileButton /> {/* Assuming you always want this in dashboards */}
