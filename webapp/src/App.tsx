772785f71d721ae2da3d9348918a49e2b8ee0c58
Andrea Cantarutti <56071114+canta2899@users.noreply.github.com>
2025-04-12 10:47:32 +0200

    feat(webapp): error boundary (#58)
    * feat: error boundary
    
    * docs: added comment to justify redirection

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -8,6 +8,8 @@ import { ApiContextProvider } from "@easymotion/auth-context";
 import { AuthContextProvider } from "@easymotion/auth-context";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterLuxon } from "@mui/x-date-pickers/AdapterLuxon";
+import { ErrorBoundary } from "react-error-boundary";
+import { GlobalErrorFallback } from "./components/fallbacks/GlobalErrorFallback";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,7 +37,9 @@ export default function App() {
                   <SnackbarCloseButton snackbarKey={snackbarKey} />
                 )}
               >
-                <Router />
+                <ErrorBoundary FallbackComponent={GlobalErrorFallback}>
+                  <Router />
+                </ErrorBoundary>
               </SnackbarProvider>
             </DialogContextProvider>
             <ReactQueryDevtools buttonPosition="bottom-left" />
