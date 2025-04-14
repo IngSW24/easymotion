d48928b83693230bd722956237b415f051604f1f
canta2899 <andreacantarutti@icloud.com>
2025-04-14 23:37:28 +0200

    feat: some email messages on sub endpoints

--- api/src/subscriptions/subscriptions.module.ts
+++ api/src/subscriptions/subscriptions.module.ts
@@ -2,8 +2,12 @@ import { Module } from "@nestjs/common";
 import { SubscriptionsService } from "./subscriptions.service";
 import { SubscriptionsController } from "./subscriptions.controller";
 import { PrismaService } from "nestjs-prisma";
+import { EmailModule } from "src/email/email.module";
+import { CourseModule } from "src/courses/courses.module";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
+  imports: [EmailModule, CourseModule, UsersModule],
   providers: [SubscriptionsService, PrismaService],
   controllers: [SubscriptionsController],
 })
