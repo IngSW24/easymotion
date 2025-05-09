60b8d9597f75d5544a1595d2d896687e35465479
PittMatteo <112318700+PittMatteo@users.noreply.github.com>
2025-05-09 15:31:24 +0200

    feat: adding health data

--- api/src/users/dto/patient/patient.dto.ts
+++ api/src/users/dto/patient/patient.dto.ts
@@ -22,6 +22,11 @@ export class PatientDto implements Patient {
   @Expose()
   smoker: boolean | null;
 
+  @ApiProperty({ description: "Alcohol consumption (integer)" })
+  @IsNumber()
+  @Expose()
+  alcohol: number | null;
+
   @ApiProperty({ description: "Activity level" })
   @IsEnum($Enums.ActivityLevel)
   @Expose()
@@ -42,10 +47,70 @@ export class PatientDto implements Patient {
   @Expose()
   bloodPressure: string | null;
 
+  @ApiProperty({ description: "Profession" })
+  @IsString()
+  @Expose()
+  profession: string | null;
+
+  @ApiProperty({ description: "Drugs" })
+  @IsString()
+  @Expose()
+  drugs: string | null;
+
+  @ApiProperty({ description: "Allergies" })
+  @IsString()
+  @Expose()
+  allergies: string | null;
+
+  @ApiProperty({ description: "Other concomitant pathologies" })
+  @IsString()
+  @Expose()
+  otherPathologies: string | null;
+
+  @ApiProperty({ description: "Pain zone" })
+  @IsString()
+  @Expose()
+  painZone: string | null;
+
+  @ApiProperty({ description: "Pain intensity" })
+  @IsNumber()
+  @Expose()
+  painIntensity: number | null;
+
+  @ApiProperty({ description: "Pain frequency" })
+  @IsEnum($Enums.PainFrequency)
+  @Expose()
+  painFrequency: $Enums.PainFrequency | null;
+
+  @ApiProperty({ description: "Pain characteristics" })
+  @IsString()
+  @Expose()
+  painCharacteristics: string | null;
+
+  @ApiProperty({ description: "Aspects that modify the intensity of pain" })
+  @IsString()
+  @Expose()
+  painModifiers: string | null;
+
+  @ApiProperty({ description: "Sleep hours" })
+  @IsNumber()
+  @Expose()
+  sleepHours: number | null;
+
+  @ApiProperty({ description: "Perceived stress" })
+  @IsNumber()
+  @Expose()
+  perceivedStress: number | null;
+
   @ApiProperty({ description: "Last medical checkup" })
   @IsDate()
   @Expose()
   lastMedicalCheckup: Date | null;
+
+  @ApiProperty({ description: "Personal goals" })
+  @IsString()
+  @Expose()
+  personalGoals: string | null;
 
   @ApiProperty({ description: "Notes" })
   @IsString()
