cc13b36763ccb7d0d532f907d5a8b36f5ebf8ead
canta2899 <andreacantarutti@icloud.com>
2025-05-18 18:30:35 +0200

    fix: polished physiotherapist unsubscribe component

--- webapp/src/components/dashboard/DeleteSubscribedUser.tsx
+++ webapp/src/components/dashboard/DeleteSubscribedUser.tsx
@@ -4,25 +4,6 @@ import React from "react";
 import { Delete } from "@mui/icons-material";
 import { useDialog } from "../../hooks/useDialog";
 
-/*
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "50%",
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-  borderRadius: 1,
-  border: "4px solid #000",
-  justifyContent: "center",
-  alignItems: "center",
-  maxHeight: "calc(100vh)", // 80% of the viewport height minus padding
-  overflowY: "auto", // enables vertical scrolling
-};
-*/
-
 export interface SubscriberDetailsProps {
   userId?: string;
   courseId?: string;
@@ -42,25 +23,15 @@ export default function DeleteSubscribedUser(props: SubscriberDetailsProps) {
     userId,
   });
 
-  const [, setOpen] = React.useState(false);
-  //const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
   const unsubscribeUser = async () => {
+    const fullName = [userFirstName, userMiddleName, userLastName]
+      .join(" ")
+      .trim()
+      .replace(/\s+/g, " ");
+
     const result = await deleteDialog.showConfirmationDialog({
       title: "Cancellazione dell'iscrizione",
-      content:
-        userMiddleName === ""
-          ? "Sei sicuro di voler disiscrivere dal corso il paziente " +
-            userFirstName +
-            " " +
-            userLastName
-          : "Sei sicuro di voler disiscrivere dal corso il paziente " +
-            userFirstName +
-            " " +
-            userMiddleName +
-            " " +
-            userLastName,
+      content: `Sei sicuro di voler disiscrivere dal corso il paziente ${fullName}`,
       confirmText: "Disiscrivi paziente",
     });
 
@@ -69,7 +40,6 @@ export default function DeleteSubscribedUser(props: SubscriberDetailsProps) {
         course_id: courseId || "",
         patient_id: userId || "",
       });
-      handleClose();
     }
   };
 
@@ -81,55 +51,3 @@ export default function DeleteSubscribedUser(props: SubscriberDetailsProps) {
     </Tooltip>
   );
 }
-
-/*
-ALTERNATIVA COL Modal:
-
-  return (
-    <>
-      <Tooltip title="Cancella iscrizione">
-        <IconButton onClick={handleOpen} color="error">
-          <Delete fontSize="small" />
-        </IconButton>
-      </Tooltip>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-      >
-        <Box sx={style}>
-          <>
-            <Box mb={5}>
-              <Typography
-                variant="h5"
-                component="h2"
-                sx={{ fontWeight: "bold" }}
-              >
-                Cancella iscrizione
-              </Typography>
-            </Box>
-            <Box mb={5}>
-              <Typography variant="body1" align="center">
-                Sei sicuro di voler disiscrivere dal corso il paziente:{" "}
-                {userFirstName} {userMiddleName} {userLastName}
-              </Typography>
-            </Box>
-
-            <Stack
-              direction={"row"}
-              spacing={2}
-              sx={{ justifyContent: "right", marginTop: 3 }}
-            >
-              <Button variant="contained" onClick={unsubscribeUser}>
-                Cancella paziente
-              </Button>
-              <Button variant="outlined" color="error" onClick={handleClose}>
-                Annulla
-              </Button>
-            </Stack>
-          </>
-        </Box>
-      </Modal>
-    </>
-
-*/
